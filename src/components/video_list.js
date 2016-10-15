e6a5a84f800d2b9a49c65044d7c2745f46db59f2
laitanop <laitanop@yahoo.com>
2016-10-15 11:15:22 -0600

    agregando punto

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -3,12 +3,9 @@ import VideoListItem from './video_list_items';
 
 const VideoList = (props) => {
 	const videoItems = props.videos.map((video) => {
-		return (
-			<VideoListItem 
-			onVideoSelect={props.onVideoSelect}
-			key={video.etag} 
-			video={video} />
-			);
+		return ( <VideoListItem onVideoSelect={props.onVideoSelect} key={video.etag} video={video} />
+
+		);
 	});
 
 	return (
